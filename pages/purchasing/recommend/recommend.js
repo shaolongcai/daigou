b009dba81930e283df0d082908ce0f2b1348043b
shaolongcai <634047622@qq.com>
2018-04-10 17:46:19 +0800

    解决关注button的函数

--- pages/purchasing/recommend/recommend.js
+++ pages/purchasing/recommend/recommend.js
@@ -22,21 +22,46 @@ Page({
    */
   onReady: function () {
     var query = new AV.Query('post');
+    const user = AV.User.current()
     query.descending('createdAt');
-    query.find().then(postData=>this.setData({postData}))
+    query.find().then(postData=>{
+      for(var i=0;i<postData.length;i++){
+        var follow = user.attributes.follow
+        var owner = postData[i].attributes.owner.id
+        var value=follow.indexOf(owner)
+        console.log(value)
+        if(value!=-1){
+          postData[i].attributes.follow = true
+        }
+        else{
+          postData[i].attributes.follow = false
+        }
+      }
+      this.setData({ postData })
+    })
   },
 
   follow:function(event){
+    console.log(event)
     // 获取帖子的主人
     var owner=event.currentTarget.dataset.owner
     const user = AV.User.current()
-    console.log(user)
     user.add("follow", owner)
     user.save().then()
 
-    this.setData({
-      follow:true
-    })
+    var postData = this.data.postData
+    for (var i = 0; i < postData.length; i++) {
+      //每篇帖子的主人
+      var owner_id = postData[i].attributes.owner.id
+      var follow = postData[i].attributes.follow
+      // 凡是帖子的主人都等于被关注人的id 则都将follow设为true
+      if (follow == false && owner_id==owner) {
+        postData[i].attributes.follow = true
+      }
+      else {}
+      this.setData({postData})
+    }
+    
     
     // 加入为粉丝 206报错 不能改变其他用户的状态，需要通过后台来操作
     // const user_id = user.id
@@ -46,7 +71,7 @@ Page({
     //   owner.add("fans",user_id)
     //   owner.save().then()
     // })
-  }
+  },
 
   
 })
